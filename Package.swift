fd58379f6e3efc214d8d51f9a7e9a7350c90063c
huos3203 <724987481@qq.com>
2016-12-24 00:57:59 +0800

    error: use of unresolved identifier 'arc4random_uniform',暂时去掉RequestLogger依赖

--- Package.swift
+++ Package.swift
@@ -50,6 +50,6 @@ let package = Package(
         .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0),
 		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 0, minor: 1),
 		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2, minor: 0),
-        .Package(url: "https://github.com/dabfleming/Perfect-RequestLogger.git", majorVersion: 0)
+//        .Package(url: "https://github.com/dabfleming/Perfect-RequestLogger.git", majorVersion: 0)
     ]
 )
